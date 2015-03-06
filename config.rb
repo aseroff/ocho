9d7bd18f876b9f4dc733625f302ca209ace79ffb
Andrew Seroff <aseroff17@gmail.com>
2015-03-05 16:42:49 -0800

    looking much better...

--- config.rb
+++ config.rb
@@ -17,7 +17,6 @@
 # page "/path/to/file.html", :layout => false
 #
 # With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
 #
 # A path which all have the same layout
 # with_layout :admin do
